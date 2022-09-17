c350c4e57964b9678b9d41c79dce74debd07b2bb
Rodrigo Silos <88406401+r-silos@users.noreply.github.com>
2022-09-16 19:14:17 -0500

    Update Hangman_game.rb

--- Hangman_game.rb
+++ Hangman_game.rb
@@ -1,4 +1,5 @@
 #this is a test to see if Github clone worked
+=begin
 Class Game
 end
 
@@ -7,7 +8,10 @@ end
 
 Class Computer
 
-    def load_in_secret_word
+    #def load_in_secret_word
         
 
 end
+=end
+contents = File.read('google-10000-english-no-swears.txt')
+puts contents
